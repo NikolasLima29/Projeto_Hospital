1595babf1de5b016fa9c77327be615a51ebfa28e
NikolasLima29 <nikolas.souzalima007@gmail.com>
2026-05-29 23:26:12 -0300

    Inserido o arquivo principal(main)

---
+++ Main.py
@@ -0,0 +1,139 @@
+import string
+            
+        
+    
+   
+    
+         
+          
+        
+    
+    
+
+
+def main():
+    i = ""
+    while i != "11":
+        print("Escolha um dos exercício")
+        print("1 - exercício 1")
+        print("5- sair")
+        i = input("Digite sua escolha: ")
+        while i == "1":
+           
+
+            i = input(
+                (
+                    "Fim do exercício, digite 1 para refazer o exercício ou enter para voltar ao menu "
+                )
+            )
+        while i == "2":
+          
+                
+            i = input(
+                (
+                    "Fim do exercício, digite 2 para refazer o exercício ou enter para voltar ao menu "
+                )
+            )
+        
+        while i == "3":
+            
+               
+               
+            i = input(
+                (
+                    "Fim do exercício, digite 3 para refazer o exercício ou enter para voltar ao menu "
+                )
+            )
+            
+        while i == "4":
+           
+            
+            i = input(
+                (
+                    "Fim do exercício, digite 4 para refazer o exercício ou enter para voltar ao menu "
+                )
+            )
+            
+        while i == "5":
+               
+               
+               i = input(
+                (
+                    "Fim do exercício, digite 5 para refazer o exercício ou enter para voltar ao menu "
+                )
+            )
+            
+           
+        while i == "6":
+               
+               
+               i = input(
+                (
+                    "Fim do exercício, digite 6 para refazer o exercício ou enter para voltar ao menu "
+                )
+            )
+           
+           
+        while i == "7":
+            
+               
+               i = input(
+                (
+                    "Fim do exercício, digite 7 para refazer o exercício ou enter para voltar ao menu "
+                )
+            )
+               
+        while i == "8":
+             
+               
+               
+               print("___________________________________________")
+               i = input(
+                (
+                    "Fim do exercício, digite 8 para refazer o exercício ou enter para voltar ao menu "
+                )
+            )
+               
+               
+               
+               
+               
+               
+               
+               
+        while i == "9":
+            
+               
+            i = input(
+                (
+                    "Fim do exercício, digite 9 para refazer o exercício ou enter para voltar ao menu "
+                )
+            )
+        
+        
+        
+        while i == "10":
+         
+            i = input(
+                (
+                    "Fim do exercício, digite 10 para refazer o exercício ou enter para voltar ao menu "
+                )
+            )
+            
+            
+
+        
+               
+        # if not i == "":
+        #     print("/////////////// ERRO ///////////////////")
+        #     print("Digite novamente, escolha apenas entre 1 a 5")
+        #     input("Digite enter para voltar ao menu")
+            
+        # else:
+        #     print("/////////////// ERRO ///////////////////")
+        #     print("Digite novamente, escolha apenas entre 1 a 5")
+        #     input("Digite enter para voltar ao menu")
+            
+
+
+main()
