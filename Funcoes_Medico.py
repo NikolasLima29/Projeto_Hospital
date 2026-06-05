4f3c707465cc0dae00aa8727aeb9a355254427ce
Gustavo Pavan Bazi <pavangustavo43@gmail.com>
2026-06-05 19:08:51 -0300

    função inserir médico quase finalizada
    Feito até os Emails

--- Funcoes_Medico.py
+++ Funcoes_Medico.py
@@ -11,32 +11,37 @@ def Incluir_Novo_Medico():
     print("3 - Incluir um novo médico:")
     print()
 
+
     # Validação do CRM do médico
-    CRM = ""
-    Tem_Letra = False
-    while len(CRM) != 6 or Tem_Letra:
+    CRM_Valido = False
+    while not CRM_Valido:
         CRM = input("Digite o CRM do médico (6 dígitos): ")
         if len(CRM) != 6:
             print("CRM inválido! Deve conter exatos 6 dígitos! ")
         else:
+
             Tem_Letra = False
             for i in range(len(CRM)):
                 if CRM[i] not in Digitos:
                     Tem_Letra = True
+
             if Tem_Letra:
                 print("CRM inválido! Contém letras e só pode ter números! ")
+            else:
+                CRM_Valido = True
+
     Medico.append(CRM)
 
+
     # Validação do nome do médico
-    Nome = ""
-    Tem_Numero = True
-    So_Espaco = True
-    while len(Nome) == 0 or Tem_Numero or So_Espaco:
+    Nome_Valido = False
+    while not Nome_Valido:
         Nome = input("Digite o nome do médico: ")
         Nome = Nome.lower()
         if len(Nome) == 0:
             print("Nome inválido! O nome não pode estar vazio!")
         else:
+
             Tem_Numero = False
             So_Espaco = True
             for i in range(len(Nome)):
@@ -44,18 +49,35 @@ def Incluir_Novo_Medico():
                     Tem_Numero = True
                 if Nome[i] != " ":
                     So_Espaco = False
+
             if Tem_Numero:
                 print("Nome Inválido! Contém números e só pode conter letras!")
             elif So_Espaco:
                 print("Nome Inválido! O nome não pode ser composto apenas por espaços!")
+            else:
+                Nome_Valido = True
+
     Nome = Nome.title()
+    Nome_sem_espacos = Nome.replace(" ", "")
+    Nome_formatado = ""
+
+    for i in range(len(Nome_sem_espacos)):
+        letra = Nome_sem_espacos[i]
+        if letra.isupper() and i > 0:
+            Nome_formatado += " " + letra
+        else:
+            Nome_formatado += letra
+
+    Nome = Nome_formatado
     Medico.append(Nome)
+
 
     # Data de nascimento do médico
     Data_De_Nascimento = ""
     Dia_De_Nascimento = ""
     Mes_De_Nascimento = ""
     Ano_De_Nascimento = ""
+
 
     # Validação do dia de nascimento do médico
     Dia_Valido = False
@@ -64,10 +86,12 @@ def Incluir_Novo_Medico():
         if len(Dia_De_Nascimento) < 1 or len(Dia_De_Nascimento) > 2:
             print("Dia inválido! O dia deve conter 1 ou 2 dígitos!")
         else:
+
             Tem_Letra = False
             for i in range(len(Dia_De_Nascimento)):
                 if Dia_De_Nascimento[i] not in Digitos:
                     Tem_Letra = True
+
             if Tem_Letra:
                 print("Dia inválido! Contém letras e só pode ter números!")
             else:
@@ -77,8 +101,10 @@ def Incluir_Novo_Medico():
                 else:
                     Dia_De_Nascimento = str(Dia_De_Nascimento)
                     Dia_Valido = True
+
     if len(Dia_De_Nascimento) == 1:
         Dia_De_Nascimento = "0" + Dia_De_Nascimento
+
 
     # Validação do mes de nascimento do médico
     Mes_Valido = False
@@ -87,10 +113,12 @@ def Incluir_Novo_Medico():
         if len(Mes_De_Nascimento) < 1 or len(Mes_De_Nascimento) > 2:
             print("Mês inválido! O mês deve conter 1 ou 2 dígitos!")
         else:
+
             Tem_Letra = False
             for i in range(len(Mes_De_Nascimento)):
                 if Mes_De_Nascimento[i] not in Digitos:
                     Tem_Letra = True
+
             if Tem_Letra:
                 print("Mês inválido! Contém letras e só pode ter números!")
             else:
@@ -99,10 +127,11 @@ def Incluir_Novo_Medico():
                     print("Mês inválido! Os meses vão de 1 a 12!")
                 else:
                     Mes_De_Nascimento = str(Mes_De_Nascimento)
-                    Mes_Valido = True
-                            
+                    Mes_Valido = True  
+
     if len(Mes_De_Nascimento) == 1:
         Mes_De_Nascimento = "0" + Mes_De_Nascimento
+
 
     # Validação do ano de nascimento do médico
     Ano_Valido = False
@@ -111,10 +140,12 @@ def Incluir_Novo_Medico():
         if len(Ano_De_Nascimento) != 4:
             print("Ano inválido! O ano deve conter exatamente 4 dígitos!")
         else:
+
             Tem_Letra = False
             for i in range(len(Ano_De_Nascimento)):
                 if Ano_De_Nascimento[i] not in Digitos:
                     Tem_Letra = True
+
             if Tem_Letra:
                 print("Ano inválido! Contém letras e só pode ter números!")
             else:
@@ -123,10 +154,176 @@ def Incluir_Novo_Medico():
                     print("Ano inválido! Digite um ano acima de 1900!")
                 else:
                     Ano_De_Nascimento = str(Ano_De_Nascimento)
-                    Ano_Valido = True
-            
+                    Ano_Valido = True    
+
     Data_De_Nascimento = Dia_De_Nascimento + "/" + Mes_De_Nascimento + "/" + Ano_De_Nascimento
     Medico.append(Data_De_Nascimento)
+
+
+    # Validação do sexo
+    Sexo_Valido = False
+    while not Sexo_Valido:
+        Sexo_Do_Medico = input("Digite o sexo do médico (M para masculino ou F para feminino): ")
+        Sexo_Do_Medico = Sexo_Do_Medico.upper()
+        if Sexo_Do_Medico != "M" and Sexo_Do_Medico != "F":
+            print("Sexo inválido! O sexo deve ser apenas M ou F!")
+        else:
+            Sexo_Valido = True
+
+    Medico.append(Sexo_Do_Medico)
+
+
+    # Validação da especialidade
+    Especialidade_Valida = False
+    while not Especialidade_Valida:
+        Especialidade = input("Digite a especialidade do médico: ")
+        Especialidade = Especialidade.lower()
+        if len(Especialidade) == 0:
+            print("Especialidade inválida! A especialidade não pode estar vazia!")
+        else:
+            Tem_Numero = False
+            So_Espaco = True
+
+            for i in range(len(Especialidade)):
+                if Especialidade[i] not in Letras:
+                    Tem_Numero = True
+                if Especialidade[i] != " ":
+                    So_Espaco = False
+            if Tem_Numero:
+                print("Especialidade Inválida! Contém números e só pode conter letras!")
+            elif So_Espaco:
+                print("Especialidade Inválida! A especialidade não pode ser composto apenas por espaços!")
+            else:
+                Especialidade_Valida = True
+
+    Especialidade = Especialidade.title()
+    Especialidade_sem_espacos = Especialidade.replace(" ", "")
+    Especialidade_formatada = ""
+
+    for i in range(len(Especialidade_sem_espacos)):
+        letra = Especialidade_sem_espacos[i]
+        if letra.isupper() and i > 0:
+            Especialidade_formatada += " " + letra
+        else:
+            Especialidade_formatada += letra
+            
+    Especialidade = Especialidade_formatada
+    Medico.append(Especialidade)
+
+
+    # Validação da universidade em que se formou
+    Universidade_Valida = False
+    while not Universidade_Valida:
+        Universidade = input("Digite a universidade do médico: ")
+        Universidade = Universidade.lower()
+        if len(Universidade) == 0:
+            print("Universidade inválida! A universidade não pode estar vazia!")
+        else:
+            Tem_Numero = False
+            So_Espaco = True
+
+            for i in range(len(Universidade)):
+                if Universidade[i] not in Letras:
+                    Tem_Numero = True
+                if Universidade[i] != " ":
+                    So_Espaco = False
+
+            if Tem_Numero:
+                print("Universidade Inválida! Contém números e só pode conter letras!")
+            elif So_Espaco:
+                print("Universidade Inválida! A universidade não pode ser composto apenas por espaços!")
+            else:
+                Universidade_Valida = True
+
+    Universidade = Universidade.title()
+    Universidade_sem_espacos = Universidade.replace(" ", "")
+    Universidade_formatada = ""
+
+    for i in range(len(Universidade_sem_espacos)):
+        letra = Universidade_sem_espacos[i]
+        if letra.isupper() and i > 0:
+            Universidade_formatada += " " + letra
+        else:
+            Universidade_formatada += letra
+
+    Universidade = Universidade_formatada
+    Medico.append(Universidade)
+
+
+    # Validação dos Emails
+    Emails_Do_Medico = []
+    Continuar = True
+    
+    while Continuar:
+        Email_Valido = False
+        while not Email_Valido:
+            Email = input("Digite o e-mail do médico: ")
+            Email = Email.lower()
+
+            Tem_Espaco = False
+            Qtd_Arroba = 0
+
+            # Conta os arrobas e verifica se tem espaços
+            for i in range(len(Email)):
+                if Email[i] == " ":
+                    Tem_Espaco = True
+                if Email[i] == "@":
+                    Qtd_Arroba += 1
+
+            if Tem_Espaco:
+                print("E-mail inválido! O e-mail não pode conter espaços.")
+            elif Qtd_Arroba != 1:
+                print("E-mail inválido! O e-mail deve conter exatamente um '@'.")
+            else:
+                # Divide a string no '@'
+                Partes = Email.split("@")
+                Usuario = Partes[0]
+                Dominio = Partes[1]
+
+                Qtd_Ponto_Dominio = 0
+                for i in range(len(Dominio)):
+                    if Dominio[i] == ".":
+                        Qtd_Ponto_Dominio += 1
+
+                if len(Usuario) < 1:
+                    print("E-mail inválido! Deve conter letras ou números antes do '@'.")
+                elif Qtd_Ponto_Dominio < 1:
+                    print("E-mail inválido! O domínio deve conter pelo menos um ponto ('.').")
+                else:
+                    # Verifica se os pontos não estão vazios (ex: falta do "a" depois do ponto)
+                    Partes_Dominio = Dominio.split(".")
+                    Dominio_Valido = True
+                    
+                    for parte in Partes_Dominio:
+                        if len(parte) == 0:
+                            Dominio_Valido = False
+
+                    if not Dominio_Valido:
+                        print("E-mail inválido! Formato incorreto (exemplo válido: a@a.a).")
+                    else:
+                        Email_Valido = True
+                        Emails_Do_Medico.append(Email)
+                        print("E-mail adicionado com sucesso!")
+
+
+        # Pergunta se o usuário quer adicionar outro e-mail
+        Opcao_Valida = False
+        while not Opcao_Valida:
+            Opcao = input("Deseja adicionar mais um e-mail? (S para Sim / N para Não): ")
+            Opcao = Opcao.upper()
+            
+            if Opcao == "S":
+                Opcao_Valida = True
+            elif Opcao == "N":
+                Opcao_Valida = True
+                Continuar = False
+            else:
+                print("Opção inválida! Digite apenas S ou N.")
+
+    Medico.append(Emails_Do_Medico)
+
+    print(Medico)
+
 
 def Main_Funcoes_Medico():
     # Ordem: Médico = (CRM, Nome, Data de Nascimento, Sexo, Especialidade, Universidade em que se formou, E-mails, Telefones) 
